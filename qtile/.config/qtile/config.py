1eb109698044d02ccfc2ed827ce1c30e571d1bf3
Kurokishin <rafaelpereirasouza15@gmail.com>
2023-04-01 22:42:41 -0300

    importing palette of colors

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -7,6 +7,7 @@ from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 from libqtile.dgroups import simple_key_binder
 from functions import *
+import color_palette
 
 # Apps
 mod = "mod4"
@@ -30,35 +31,7 @@ black = "#000000"
 gray = "#505050"
 pink = "#ffc0cb"
 purple = "#f3b9ff"
-
-catppuccin_mocha = {
-    "rosewater": "#f5e0dc",
-    "flamingo": "#f2cdcd",
-    "pink": "#f5c2e7",
-    "mauve": "#cba6f7",
-    "red": "#f38ba8",
-    "maroon": "#eba0ac",
-    "peach": "#fab387",
-    "yellow": "#f9e2af",
-    "green": "#a6e3a1",
-    "teal": "#94e2d5",
-    "sky": "#89dceb",
-    "sapphire": "#74c7ec",
-    "blue": "#89b4fa",
-    "lavender": "#b4befe",
-    "text": "#cdd6f4",
-    "subtext1": "#bac2de",
-    "subtext0": "#a6adc8",
-    "overlay2": "#9399b2",
-    "overlay1": "#7f849c",
-    "overlay0": "#6c7086",
-    "surface2": "#585b70",
-    "surface1": "#45475a",
-    "surface0": "#313244",
-    "base": "#1e1e2e",
-    "mantle": "#181825",
-    "crust": "#11111b",
-}
+catppuccin_mocha = color_palette.catppuccin
 
 keys = [
     # Switch between monitors
